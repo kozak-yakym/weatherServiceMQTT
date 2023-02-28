f50b21e13c733522ab9a360ed3a661fde3dfb3b8
Andrii <anyak@ciklum.com>
2023-02-28 19:39:31 +0100

    Code clean-up.

--- src/services.js
+++ src/services.js
@@ -6,15 +6,11 @@
 //=============================================
 var dateFormat = require('dateformat');
 var weather = require('openweather-apis');
-// TODO: Move this to config file with examples.
 
 function servicesM(service_creds) {
-  // var template;
-
-  // Device config parse and MQTT connectivity
+  // Device config parse and MQTT connectivity.
   var mqtt = require('mqtt'), url = require('url');
 
-  // protocol: "mqtts",
   var options = {
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
     host: service_creds.clientURL,
@@ -41,10 +37,8 @@ function servicesM(service_creds) {
   console.log('clientId: \n', options.clientId);
   console.log('topics: \n', time_date_topic, '\n', weather_topic, '\n', weather_forecast_topic);
 
-  //
   // servicem is an instance returned by mqtt.Client(), see mqtt.js for full
   // documentation.
-  //
   servicem
     .on('connect', function() {
       console.log('connect');
@@ -72,9 +66,6 @@ function servicesM(service_creds) {
 
   // Weather config ------------------------------------
   // set country by ID
-  // weather.setLang('ua');
-  
-  // set country by ID
   weather.setLang('de');
 
   // TODO: Move this to config file with examples.
@@ -88,71 +79,6 @@ function servicesM(service_creds) {
   // check http://openweathermap.org/appid#get for get the APPID
   weather.setAPPID(service_creds.weatherAPIkey);
 
-/*    
-  // Payload composing 
-  var me = this;
-  var model = {};
-
-  // var id = getDeviceId(connectionString);
-  function clone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-  } //!FIXME
-
-  for (var key in template) {
-    model[key] = {
-      model: me,
-      key: key,
-      type: template[key].type,
-      fs: template[key].fs,
-      update: template[key].update
-    };
-    if (template[key].value)
-      model[key].value = clone(template[key].value);
-    if (template[key].data)
-      model[key].data = clone(template[key].data);
-
-    if (model[key].type === 'stream') {
-      model[key].index = 0;
-      model[key].frame_size = template[key].frame_size;
-    }
-  }
-
-  // this.getDeviceId = getDeviceId; 
-
-  this.getData = function(t) {
-    var d = {};
-
-    for (var key in model) {
-      if (!model[key].t)
-        model[key].t = t;
-      switch (model[key].type) {
-        case 'value':
-        case 'vector':
-        case 'text':
-          if ((t - model[key].t) * 0.001 < 1.0 / model[key].fs)
-            break;
-          model[key].t += 1000.0 / model[key].fs;
-          if (typeof(model[key].update) === 'function')
-            model[key].value = model[key].update(model[key], t);
-          d[key] = model[key].value;
-          break;
-
-        case 'stream':
-          if ((t - model[key].t) * 0.001 < 1.0 / model[key].fs * model[key].frame_size)
-            break;
-          var buf = [];
-          if (typeof(model[key].update) === 'function') {
-            buf = model[key].update(model[key], model[key].t, model[key].index);
-          }  
-          d[key] = { t: model[key].t, index: model[key].index, buf: buf };
-          model[key].index += buf.length;
-          model[key].t += 1000.0 / model[key].fs * buf.length;
-          break;
-        }
-    }
-    return d;
-  };
-*/
   var timer = setInterval(function() {
 // Datetime-------------------------------------------------------
     var t = new Date().getTime();
@@ -175,26 +101,6 @@ function servicesM(service_creds) {
 // ---------------------------------------------------------------
 
 // Weather--------------------------------------------------------
-    // // get the Temperature  
-    // weather.getTemperature(function(err, temp){
-    //   console.log(temp);
-    // });
-  
-    // // get the Atm Pressure
-    // weather.getPressure(function(err, pres){
-    //     console.log(pres);
-    // });
-  
-    // // get the Humidity
-    // weather.getHumidity(function(err, hum){
-    //     console.log(hum);
-    // });
-  
-    // // get the Description of the weather condition
-    // weather.getDescription(function(err, desc){
-    //     console.log(desc);
-    // });
-  
     // get all the JSON file returned from server (rich of info)
     weather.getAllWeather(function(err, JSONObj){
       // console.log(JSONObj);
